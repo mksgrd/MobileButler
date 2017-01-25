1a001aa06f7950500391fe7777818bbe4352e253
Maksim <maksgord-152@yandex.ru>
2017-01-25 18:00:08 +0300

    Created simple test for Wifi

--- app/src/main/java/com/maksg/mobilebutler/MainActivity.java
+++ app/src/main/java/com/maksg/mobilebutler/MainActivity.java
@@ -2,12 +2,75 @@ package com.maksg.mobilebutler;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import wifiSwitcher.WifiConnector;
 
 public class MainActivity extends AppCompatActivity {
+    private int type = 0;
+    private WifiConnector connector = new WifiConnector();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText name = (EditText) findViewById(R.id.netNameTE);
+        final EditText pass = (EditText) findViewById(R.id.netPassTE);
+
+        name.setText("Smart_boxGAS");
+        pass.setText("stopandgo1904");
+
+        connector.setContext(this);
+
+        RadioGroup group = (RadioGroup) findViewById(R.id.radioGroup);
+        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case -1:
+                        Toast.makeText(getApplicationContext(), "Ничего не выбрано",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.wepRbutton:
+                        type = WifiConnector.NETWORK_TYPE_WEP;
+                        break;
+                    case R.id.wpaRbutton:
+                        type = WifiConnector.NETWORK_TYPE_WPA;
+                        break;
+                    case R.id.openRbutton:
+                        type = WifiConnector.NETWORK_TYPE_OPEN;
+                        break;
+
+                }
+            }
+        });
+
+        final Button connectButton = (Button) findViewById(R.id.connectButton);
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                connector.connect(name.getText().toString(), pass.getText().toString(), type);
+            }
+        });
+
+        Button enableWifiButton = (Button) findViewById(R.id.enableWifiButton);
+        enableWifiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                connector.setWifiState(true);
+            }
+        });
+
+        Button disableWifiButton = (Button) findViewById(R.id.disableWifiButton);
+        disableWifiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                connector.setWifiState(false);
+            }
+        });
     }
 
 }
