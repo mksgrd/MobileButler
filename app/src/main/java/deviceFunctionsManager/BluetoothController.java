17243f9e365ac56775660dbf404d455edd3c0c92
Maksim <maksgord-152@yandex.ru>
2017-02-12 14:54:48 +0300

    Added Bluetooth controller

---
+++ app/src/main/java/deviceFunctionsManager/BluetoothController.java
@@ -0,0 +1,20 @@
+package deviceFunctionsManager;
+
+import android.bluetooth.BluetoothAdapter;
+
+public class BluetoothController {
+    private BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+    public BluetoothController() {
+        if (bluetoothAdapter == null)
+            throw new RuntimeException("Device does not support Bluetooth");
+    }
+
+    public void setBluetoothState(boolean state) {
+        boolean isEnabled = bluetoothAdapter.isEnabled();
+        if (state && !isEnabled)
+            bluetoothAdapter.enable();
+        else if (!state && isEnabled)
+            bluetoothAdapter.disable();
+    }
+}
--- app/src/main/java/deviceFunctionsManager/ControllersInitializer.java
+++ app/src/main/java/deviceFunctionsManager/ControllersInitializer.java
@@ -2,9 +2,10 @@ package deviceFunctionsManager;
 
 import android.content.Context;
 
-public class Controllers {
+public class ControllersInitializer {
     private AudioController audioController = new AudioController();
     private WifiController wifiController = new WifiController();
+    private BluetoothController bluetoothController = new BluetoothController();
 
     public void setContext(Context context) {
         audioController.setContext(context);
@@ -17,5 +18,9 @@ public class Controllers {
 
     public WifiController getWifiController() {
         return wifiController;
+    }
+
+    public BluetoothController getBluetoothController() {
+        return bluetoothController;
     }
 }
