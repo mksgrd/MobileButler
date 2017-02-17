75bc55facdbc52cba7c74f8c2e6a91b2701dc6a7
Maksim <maksgord-152@yandex.ru>
2017-02-17 18:08:29 +0300

    Remove useless code

--- app/src/main/java/scheduler/EventScheduler.java
+++ app/src/main/java/scheduler/EventScheduler.java
@@ -1,17 +1,13 @@
 package scheduler;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Timer;
 
 public class EventScheduler {
     private final Timer timer = new Timer();
-    private final ArrayList<Event> scheduledEvents = new ArrayList<>();
     private final Calendar currentMoment = Calendar.getInstance();
 
     public void scheduleEvent(Event event) {
-        scheduledEvents.add(event);
-
         long waitUntilRun = event.getStartMoment().getTimeInMillis() - currentMoment.getTimeInMillis();
 
         if (waitUntilRun <= 0)
@@ -24,15 +20,13 @@ public class EventScheduler {
     }
 
     public void cancelEvent(Event event) {
-        scheduledEvents.remove(event);
         event.cancel();
         timer.purge();
     }
 
-    public void cancelAllEvents() {
-        for (Event scheduledEvent : scheduledEvents)
-            scheduledEvent.cancel();
-        scheduledEvents.clear();
+    public void cancelAllEvents(Event... events) {
+        for (Event event : events)
+            event.cancel();
         timer.purge();
     }
 }
