41ad4d2b06240b94e15f501adffcddd19d1cb2fb
Maksim <maksgord-152@yandex.ru>
2017-02-24 20:36:21 +0300

    Added ListView in ScheduleEventActivity

--- app/src/main/java/com/maksg/mobilebutler/ScheduleEventActivity.java
+++ app/src/main/java/com/maksg/mobilebutler/ScheduleEventActivity.java
@@ -2,6 +2,8 @@ package com.maksg.mobilebutler;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ScheduleEventActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class ScheduleEventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_event);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        final String[] names = new String[]{"Управление звуком", "Управление Wi-Fi", "Управление Bluetooth",
+                "Управление связью"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, names);
+        listView.setAdapter(adapter);
     }
 }
