358d12b8d0d0d630fe05ed143e8e610b1084cae4
Maksim <maksgord-152@yandex.ru>
2017-02-24 11:20:58 +0300

    Added other activities and OnClick method

--- app/src/main/java/com/maksg/mobilebutler/MainActivity.java
+++ app/src/main/java/com/maksg/mobilebutler/MainActivity.java
@@ -1,12 +1,34 @@
 package com.maksg.mobilebutler;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onScheduleButtonClick(View view) {
+        Intent intent = new Intent(this, ScheduleEventActivity.class);
+        startActivity(intent);
+    }
+
+    public void onTaskButtonClick(View view) {
+        Intent intent = new Intent(this, ScheduleTaskActivity.class);
+        startActivity(intent);
+    }
+
+    public void onCurrentProfileButtonClick(View view) {
+        Intent intent = new Intent(this, CurrentProfileActivity.class);
+        startActivity(intent);
+    }
+
+    public void onConfigureProfileButtonClick(View view) {
+        Intent intent = new Intent(this, CurrentProfileActivity.class);
+        startActivity(intent);
     }
 }
