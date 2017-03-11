94f9fafd10394e7a435fe6a7d90f782d07541ca5
Maksim <maksgord-152@yandex.ru>
2017-03-11 20:25:40 +0300

    Added ChangeSoundSettingsActivity

--- app/src/main/java/com/maksg/mobilebutler/ChooseActionActivity.java
+++ app/src/main/java/com/maksg/mobilebutler/ChooseActionActivity.java
@@ -1,12 +1,19 @@
 package com.maksg.mobilebutler;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class ChooseActionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_action);
+    }
+
+    public void onChangeSoundSettingsButtonClick(View view) {
+        Intent intent = new Intent(this, ChangeSoundSettingsActivity.class);
+        startActivity(intent);
     }
 }
